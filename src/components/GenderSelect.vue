ffb299dfd235ec6096ae9d45b3bf7fad6c02c5f6
Molnar József(Pepó) <mojo12984@gmail.com>
2021-02-04 09:24:41 +0100

    Create GenderSelect.vue

---
+++ src/components/GenderSelect.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <div class="row text-center">
+      <div class="col">
+        <button
+          :class="['btn', isSelectedM ? 'btn-success' : 'btn-primary']"
+          @click="onMaleSelect"
+        >
+          Male
+        </button>
+      </div>
+      <div class="col">
+        <button
+          :class="['btn', isSelectedF ? 'btn-success' : 'btn-primary']"
+          @click="onFemaleSelect"
+        >
+          Female
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { types as mutationTypes } from "@/store/mutations";
+
+export default {
+  name: "GenderSelect",
+  data() {
+    return { isSelectedM: false, isSelectedF: false, selectedGender: "" };
+  },
+  methods: {
+    onMaleSelect() {
+      this.selectedGender = "Male";
+      this.isSelectedM = true;
+      this.isSelectedF = false;
+      this.$store.commit(mutationTypes.SET_GENDER, this.selectedGender);
+    },
+    onFemaleSelect() {
+      this.selectedGender = "Female";
+      this.isSelectedM = false;
+      this.isSelectedF = true;
+      this.$store.commit(mutationTypes.SET_GENDER, this.selectedGender);
+    }
+  }
+};
+</script>
+
+<style scoped></style>
