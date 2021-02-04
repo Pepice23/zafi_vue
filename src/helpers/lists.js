3692a54cb2b74ea44838d71766028d02923e0a63
Molnar József(Pepó) <mojo12984@gmail.com>
2021-02-04 09:19:49 +0100

    added helpers

---
+++ src/helpers/lists.js
@@ -0,0 +1,383 @@
+export const allClasses = [
+  "Warrior",
+  "Hunter",
+  "Death Knight",
+  "Mage",
+  "Priest",
+  "Monk",
+  "Rogue",
+  "Warlock",
+  "Paladin",
+  "Druid",
+  "Demon Hunter",
+  "Shaman"
+];
+
+const human = {
+  name: "Human",
+  availableClasses: [
+    "Warrior",
+    "Hunter",
+    "Death Knight",
+    "Mage",
+    "Priest",
+    "Monk",
+    "Rogue",
+    "Warlock",
+    "Paladin"
+  ],
+  faction: "Alliance"
+};
+
+const dwarf = {
+  name: "Dwarf",
+  availableClasses: [
+    "Warrior",
+    "Hunter",
+    "Death Knight",
+    "Mage",
+    "Priest",
+    "Monk",
+    "Rogue",
+    "Warlock",
+    "Shaman",
+    "Paladin"
+  ],
+  faction: "Alliance"
+};
+const nightElf = {
+  name: "Night Elf",
+  availableClasses: [
+    "Warrior",
+    "Hunter",
+    "Death Knight",
+    "Mage",
+    "Priest",
+    "Monk",
+    "Rogue",
+    "Druid",
+    "Demon Hunter"
+  ],
+  faction: "Alliance"
+};
+const gnome = {
+  name: "Gnome",
+  availableClasses: [
+    "Warrior",
+    "Hunter",
+    "Death Knight",
+    "Mage",
+    "Priest",
+    "Monk",
+    "Rogue",
+    "Warlock"
+  ],
+  faction: "Alliance"
+};
+const draenei = {
+  name: "Draenei",
+  availableClasses: [
+    "Warrior",
+    "Hunter",
+    "Death Knight",
+    "Mage",
+    "Priest",
+    "Monk",
+    "Shaman",
+    "Paladin"
+  ],
+  faction: "Alliance"
+};
+const worgen = {
+  name: "Worgen",
+  availableClasses: [
+    "Warrior",
+    "Hunter",
+    "Death Knight",
+    "Mage",
+    "Priest",
+    "Rogue",
+    "Warlock",
+    "Druid"
+  ],
+  faction: "Alliance"
+};
+const voidElf = {
+  name: "Void Elf",
+  availableClasses: [
+    "Warrior",
+    "Hunter",
+    "Death Knight",
+    "Mage",
+    "Priest",
+    "Monk",
+    "Rogue",
+    "Warlock"
+  ],
+  faction: "Alliance"
+};
+const lightForgedDraenei = {
+  name: "Lightforged Draenei",
+  availableClasses: ["Warrior", "Hunter", "Death Knight", "Mage", "Priest"],
+  faction: "Alliance"
+};
+const darkIronDwarf = {
+  name: "Dark Iron Dwarf",
+  availableClasses: [
+    "Warrior",
+    "Hunter",
+    "Death Knight",
+    "Mage",
+    "Priest",
+    "Paladin"
+  ],
+  faction: "Alliance"
+};
+const kulTiranHuman = {
+  name: "Kultiran Human",
+  availableClasses: [
+    "Warrior",
+    "Hunter",
+    "Death Knight",
+    "Mage",
+    "Priest",
+    "Monk",
+    "Rogue",
+    "Shaman",
+    "Druid"
+  ],
+  faction: "Alliance"
+};
+const mechagnome = {
+  name: "Mechagnome",
+  availableClasses: [
+    "Warrior",
+    "Hunter",
+    "Death Knight",
+    "Mage",
+    "Priest",
+    "Monk",
+    "Rogue",
+    "Warlock"
+  ],
+  faction: "Alliance"
+};
+
+const orc = {
+  name: "Orc",
+  availableClasses: [
+    "Warrior",
+    "Hunter",
+    "Death Knight",
+    "Mage",
+    "Monk",
+    "Rogue",
+    "Warlock",
+    "Shaman"
+  ],
+  faction: "Horde"
+};
+const undead = {
+  name: "Undead",
+  availableClasses: [
+    "Warrior",
+    "Hunter",
+    "Death Knight",
+    "Mage",
+    "Priest",
+    "Monk",
+    "Rogue",
+    "Warlock"
+  ],
+  faction: "Horde"
+};
+const tauren = {
+  name: "Tauren",
+  availableClasses: [
+    "Warrior",
+    "Hunter",
+    "Death Knight",
+    "Priest",
+    "Monk",
+    "Shaman",
+    "Paladin",
+    "Druid"
+  ],
+  faction: "Horde"
+};
+const troll = {
+  name: "Troll",
+  availableClasses: [
+    "Warrior",
+    "Hunter",
+    "Death Knight",
+    "Mage",
+    "Priest",
+    "Monk",
+    "Rogue",
+    "Warlock",
+    "Shaman",
+    "Druid"
+  ],
+  faction: "Horde"
+};
+const bloodElf = {
+  name: "Blood Elf",
+  availableClasses: [
+    "Warrior",
+    "Hunter",
+    "Death Knight",
+    "Mage",
+    "Priest",
+    "Monk",
+    "Rogue",
+    "Warlock",
+    "Paladin",
+    "Demon Hunter"
+  ],
+  faction: "Horde"
+};
+const goblin = {
+  name: "Goblin",
+  availableClasses: [
+    "Warrior",
+    "Hunter",
+    "Death Knight",
+    "Mage",
+    "Priest",
+    "Rogue",
+    "Warlock",
+    "Shaman"
+  ],
+  faction: "Horde"
+};
+const nightBorne = {
+  name: "Nightborne",
+  availableClasses: [
+    "Warrior",
+    "Hunter",
+    "Death Knight",
+    "Mage",
+    "Priest",
+    "Monk",
+    "Rogue",
+    "Warlock"
+  ],
+  faction: "Horde"
+};
+const highmountainTauren = {
+  name: "Highmountain Tauren",
+  availableClasses: [
+    "Warrior",
+    "Hunter",
+    "Death Knight",
+    "Mage",
+    "Priest",
+    "Monk",
+    "Shaman",
+    "Druid"
+  ],
+  faction: "Horde"
+};
+const magHarOrc = {
+  name: 'Mag"har Orc',
+  availableClasses: [
+    "Warrior",
+    "Hunter",
+    "Death Knight",
+    "Mage",
+    "Priest",
+    "Monk",
+    "Rogue",
+    "Shaman"
+  ],
+  faction: "Horde"
+};
+const zandalariTroll = {
+  name: "Zandalari Troll",
+  availableClasses: [
+    "Warrior",
+    "Hunter",
+    "Death Knight",
+    "Mage",
+    "Priest",
+    "Monk",
+    "Rogue",
+    "Shaman",
+    "Paladin",
+    "Druid"
+  ],
+  faction: "Horde"
+};
+const vulpera = {
+  name: "Vulpera",
+  availableClasses: [
+    "Warrior",
+    "Hunter",
+    "Death Knight",
+    "Mage",
+    "Priest",
+    "Monk",
+    "Rogue",
+    "Warlock",
+    "Shaman"
+  ],
+  faction: "Horde"
+};
+const pandarenA = {
+  name: "Pandaren(A)",
+  availableClasses: [
+    "Warrior",
+    "Hunter",
+    "Death Knight",
+    "Mage",
+    "Priest",
+    "Monk",
+    "Rogue",
+    "Shaman"
+  ],
+  faction: "Alliance"
+};
+const pandarenH = {
+  name: "Pandaren(H)",
+  availableClasses: [
+    "Warrior",
+    "Hunter",
+    "Death Knight",
+    "Mage",
+    "Priest",
+    "Monk",
+    "Rogue",
+    "Shaman"
+  ],
+  faction: "Horde"
+};
+export const alliance = [
+  human,
+  dwarf,
+  nightElf,
+  gnome,
+  draenei,
+  worgen,
+  pandarenA,
+  voidElf,
+  darkIronDwarf,
+  lightForgedDraenei,
+  kulTiranHuman,
+  mechagnome
+];
+
+export const horde = [
+  orc,
+  undead,
+  tauren,
+  troll,
+  bloodElf,
+  goblin,
+  pandarenH,
+  nightBorne,
+  highmountainTauren,
+  magHarOrc,
+  zandalariTroll,
+  vulpera
+];
