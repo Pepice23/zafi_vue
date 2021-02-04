684804a44c532724cc2eb226f1658150d2721309
Molnar József(Pepó) <mojo12984@gmail.com>
2021-02-04 14:29:07 +0100

    Create CharacterList.vue

---
+++ src/views/CharacterList.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container-fluid">
+    itt lesz a karakter lista
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CharacterList"
+};
+</script>
+
+<style scoped></style>
