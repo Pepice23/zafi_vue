9605947fca8585b6b4d2342a6e1aa3e3293fdd38
Molnar József(Pepó) <mojo12984@gmail.com>
2021-02-04 09:23:45 +0100

    Create Flag.vue

---
+++ src/components/Flag.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="text-center">
+    <div class="row  mt-1">
+      <div class="col">
+        <img
+          src="../assets/logos/alliance-logo.png"
+          alt="alliance"
+          class="flag"
+        />
+      </div>
+      <div class="col">
+        <img src="../assets/logos/wow-logo.jpg" alt="wow" class="logo" />
+      </div>
+      <div class="col">
+        <img src="../assets/logos/horde-logo.png" alt="horde" class="flag" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Flag"
+};
+</script>
+
+<style scoped>
+.flag {
+  width: 200px;
+  height: 200px;
+}
+.logo {
+  width: 400px;
+  height: 200px;
+}
+</style>
