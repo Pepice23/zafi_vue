06e072e04c8ef30444cd1697a4c49618d9f64cda
Molnar József(Pepó) <mojo12984@gmail.com>
2021-02-04 09:29:19 +0100

    Create CharacterPanelPreview.vue

---
+++ src/components/CharacterPanelPreview.vue
@@ -0,0 +1,20 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "CharacterPanelPreview"
+};
+</script>
+
+<style scoped>
+.miniFlag {
+  width: 50px;
+  height: 50px;
+}
+.crest {
+  width: 200px;
+  height: 200px;
+}
+</style>
