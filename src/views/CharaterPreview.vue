ed7ff794ae4687e466082a9226d7d6def5a68282
Molnar József(Pepó) <mojo12984@gmail.com>
2021-02-04 13:43:09 +0100

    added character preview

---
+++ src/views/CharaterPreview.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container-fluid text-center">
+    <h6>Az elkészült Karakter:</h6>
+    <CharacterPanelPreview />
+    <button class="btn btn-primary" @click="characterReady">
+      Mehet a játék!
+    </button>
+  </div>
+</template>
+
+<script>
+import CharacterPanelPreview from "@/components/CharacterPanelPreview";
+import { types as actionTypes } from "@/store/actions";
+export default {
+  name: "CharaterPreview",
+  components: { CharacterPanelPreview },
+  methods: {
+    characterReady() {
+      this.$store.dispatch(actionTypes.ADD_NEW_CHARACTER_TO_DB);
+    }
+  }
+};
+</script>
+
+<style scoped></style>
